0d3a303b8b478c45cf074556956f47a29267cc59
saifaldin14 <saifaldindeveloper@gmail.com>
2021-07-09 12:46:10 -0400

    Create tweets.ts

---
+++ constants/data/tweets.ts
@@ -0,0 +1,21 @@
+const tweets = [
+  {
+    id: "t1",
+    user: {
+      id: "u1",
+      username: "Saif",
+      name: "Saif Al-Din",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/1/19/Águila_calva.jpg",
+    },
+    createdAt: "2021-07-09T12:00:00.000Z",
+    content: "lorem ipsum",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/1/19/Águila_calva.jpg",
+    numberOfComments: 12,
+    numberOfRetweets: 3,
+    numberOfLikes: 10,
+  },
+];
+
+export default tweets;
