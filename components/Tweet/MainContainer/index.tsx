001fc635b41b0a4851dc6b951007319b7639c6cb
saifaldin14 <saifaldindeveloper@gmail.com>
2021-07-08 22:36:29 -0400

    Update index.tsx

--- components/Tweet/MainContainer/index.tsx
+++ components/Tweet/MainContainer/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 export type MainContainerProps = {};
 
 const MainContainer = (props: MainContainerProps) => (
   <View>
+    <Text>Hello World</Text>
     {/*  Username  */}
     {/*  Content  */}
     {/*  Tweet Footer  */}
