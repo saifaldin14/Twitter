03ad20504279c479b015cf8a3d37609c671575c0
saifaldin14 <saifaldindeveloper@gmail.com>
2021-07-13 16:13:49 -0400

    Update feed

--- components/Feed/index.tsx
+++ components/Feed/index.tsx
@@ -4,11 +4,13 @@ import tweets from "../../constants/data/tweets";
 import Tweet from "../Tweet";
 
 const Feed = () => (
-  <FlatList
-    data={tweets}
-    renderItem={({ item }) => <Tweet tweet={item} />}
-    keyExtractor={(item) => item.id}
-  />
+  <View style={{ width: "100%" }}>
+    <FlatList
+      data={tweets}
+      renderItem={({ item }) => <Tweet tweet={item} />}
+      keyExtractor={(item) => item.id}
+    />
+  </View>
 );
 
 export default Feed;
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -3,13 +3,12 @@ import { StyleSheet } from "react-native";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
-import Tweet from "../components/Tweet";
-import tweets from "../constants/data/tweets";
+import Feed from "../components/Feed";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Tweet tweet={tweets[0]} />
+      <Feed />
     </View>
   );
 }
