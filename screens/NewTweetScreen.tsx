288e4af48210f0cee6d7f2f482fd0fb4aa4c875a
saifaldin14 <saifaldindeveloper@gmail.com>
2021-07-13 16:46:52 -0400

    Update NewTweetScreen.tsx

--- screens/NewTweetScreen.tsx
+++ screens/NewTweetScreen.tsx
@@ -1,6 +1,13 @@
 import { AntDesign } from "@expo/vector-icons";
 import * as React from "react";
-import { StyleSheet, TouchableOpacity, Text } from "react-native";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  SafeAreaView,
+  TextInput,
+} from "react-native";
+import ProfilePicture from "../components/ProfilePicture";
 import { View } from "../components/Themed";
 import Colors from "../constants/Colors";
 
@@ -9,22 +16,45 @@ export default function NewTweetScreen() {
     console.log("Hi");
   };
   return (
-    <View style={styles.container}>
-      <View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerContainer}>
         <AntDesign name={"close"} size={30} color={Colors.light.tint} />
         <TouchableOpacity style={styles.button} onPress={onPostTweet}>
           <Text style={styles.buttonText}>Tweet</Text>
         </TouchableOpacity>
       </View>
-    </View>
+      <View style={styles.newTweetContainer}>
+        <ProfilePicture
+          image={
+            "https://upload.wikimedia.org/wikipedia/commons/1/19/Águila_calva.jpg"
+          }
+        />
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.tweetInput}
+            placeholder={"Share what's on your mind"}
+          />
+          <TextInput
+            style={styles.imageInput}
+            placeholder={"Image URL optional"}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: "flex-start",
+    backgroundColor: "white",
+  },
+  headerContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 15,
   },
   button: {
     backgroundColor: Colors.light.tint,
@@ -37,4 +67,17 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 16,
   },
+  newTweetContainer: {
+    flexDirection: "row",
+    padding: 15,
+  },
+  inputContainer: {
+    marginLeft: 10,
+  },
+  tweetInput: {
+    height: 100,
+    maxHeight: 300,
+    fontSize: 20,
+  },
+  imageInput: {},
 });
