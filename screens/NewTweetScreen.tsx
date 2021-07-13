8ddbb484bf35bd12020f34b6e2fb4d2c09b4dba3
saifaldin14 <saifaldindeveloper@gmail.com>
2021-07-13 16:35:40 -0400

    Update NewTweetScreen.tsx

--- screens/NewTweetScreen.tsx
+++ screens/NewTweetScreen.tsx
@@ -1,16 +1,21 @@
+import { AntDesign } from "@expo/vector-icons";
 import * as React from "react";
-import { StyleSheet } from "react-native";
-
-import EditScreenInfo from "../components/EditScreenInfo";
+import { StyleSheet, TouchableOpacity, Text } from "react-native";
 import { View } from "../components/Themed";
-import Feed from "../components/Feed";
-import NewTweetButton from "../components/NewTweetButton";
+import Colors from "../constants/Colors";
 
 export default function NewTweetScreen() {
+  const onPostTweet = () => {
+    console.log("Hi");
+  };
   return (
     <View style={styles.container}>
-      <Feed />
-      <NewTweetButton />
+      <View>
+        <AntDesign name={"close"} size={30} color={Colors.light.tint} />
+        <TouchableOpacity style={styles.button} onPress={onPostTweet}>
+          <Text style={styles.buttonText}>Tweet</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -20,5 +25,16 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    backgroundColor: Colors.light.tint,
+    borderRadius: 30,
+  },
+  buttonText: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
   },
 });
