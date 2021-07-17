7d18e4d5661f4fb7610e329f6ca11ca36f042919
saifaldin14 <saifaldindeveloper@gmail.com>
2021-07-17 15:47:10 -0400

    Check if user exists and save them to DB

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
-import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import Amplify, { Auth, API, graphqlOperation } from "aws-amplify";
 import { withAuthenticator } from "aws-amplify-react-native";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
-import Amplify, { Auth, API, graphqlOperation } from "aws-amplify";
 import config from "./aws-exports";
+
+import { getUser } from "./src/graphql/queries";
+import { createUser } from "./src/graphql/mutations";
 
 Amplify.configure(config);
 
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const getRandomImage = () => {
+    return "https://scontent.fkiv3-1.fna.fbcdn.net/v/t31.0-8/s960x960/22256588_1932617800312085_5686197942193420542_o.jpg?_nc_cat=110&_nc_sid=85a577&_nc_ohc=svjjE7DUkc0AX9yjcdC&_nc_ht=scontent.fkiv3-1.fna&tp=7&oh=1df4116c73c45a32ebad070704ca3333&oe=5F6ECD77";
+  };
+
+  const saveUserToDB = async (user) => {
+    await API.graphql(graphqlOperation(createUser, { input: user }));
+  };
 
   useEffect(() => {
     const updateUser = async () => {
@@ -25,11 +35,26 @@ function App() {
 
       if (userInfo) {
         // Check if user already exists in database
+        const userData = await API.graphql(
+          graphqlOperation(getUser, { id: userInfo.attributes.sub })
+        );
+        if (!userData.data.getUser) {
+          const user = {
+            id: userInfo.attributes.sub,
+            username: userInfo.username,
+            name: userInfo.username,
+            email: userInfo.attributes.email,
+            image: getRandomImage(),
+          };
+          await saveUserToDB(user);
+        } else {
+          console.log("User already exists");
+        }
       }
     };
 
     updateUser();
-  });
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
