9450d09986589c1dcbbd3ba147edc9d405e67fc5
saifaldin14 <saifaldindeveloper@gmail.com>
2021-07-16 20:06:00 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { withAuthenticator } from "aws-amplify-react-native";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
-import Amplify from "aws-amplify";
+import Amplify, { Auth, API, graphqlOperation } from "aws-amplify";
 import config from "./aws-exports";
 
 Amplify.configure(config);
@@ -15,6 +15,21 @@ Amplify.configure(config);
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    const updateUser = async () => {
+      // Get current authenticated user
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+
+      if (userInfo) {
+        // Check if user already exists in database
+      }
+    };
+
+    updateUser();
+  });
 
   if (!isLoadingComplete) {
     return null;
