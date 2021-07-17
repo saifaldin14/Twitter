2bf2c41ce997d6009637026a2e884d48d9938fc5
saifaldin14 <saifaldindeveloper@gmail.com>
2021-07-17 15:51:21 -0400

    Update HomeScreen.tsx

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
 import { StyleSheet } from "react-native";
 
-import EditScreenInfo from "../components/EditScreenInfo";
-import { View } from "../components/Themed";
+import { Text, View } from "../components/Themed";
 import Feed from "../components/Feed";
 import NewTweetButton from "../components/NewTweetButton";
 
